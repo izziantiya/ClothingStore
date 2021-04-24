ccc78490ba4046ddf81af506c5272efe5ea5c270
Elizaveta <izziantiya@gmail.com>
2021-04-24 10:32:00 +0300

    Add: products on the main page from db

--- src/main/java/com/store/controller/DefaultController.java
+++ src/main/java/com/store/controller/DefaultController.java
@@ -1,11 +1,17 @@
 package com.store.controller;
 
+import com.store.entity.Product;
 import com.store.entity.ProductType;
+import com.store.repository.ProductRepository;
 import com.store.repository.ProductTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class DefaultController {
@@ -13,10 +19,15 @@ public class DefaultController {
     @Autowired
     ProductTypeRepository productTypeRepository;
 
+    @Autowired
+    ProductRepository productRepository;
+
     @GetMapping("/")
     public String index(Model model) {
         Iterable<ProductType> types = productTypeRepository.findAll();
-        model.addAttribute("types", types);
+        Map<ProductType, List<Product>> map = new HashMap<>();
+        types.forEach(type -> map.put(type, productRepository.findByProductType(type)));
+        model.addAttribute("map", map);
         return "index";
     }
 }
--- src/main/java/com/store/repository/ProductRepository.java
+++ src/main/java/com/store/repository/ProductRepository.java
@@ -1,10 +1,14 @@
 package com.store.repository;
 
 import com.store.entity.Product;
+import com.store.entity.ProductType;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
+    public List<Product>  findByProductType(ProductType productType);
 }

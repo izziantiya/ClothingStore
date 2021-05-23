d71222a948a5e9e9bb2ccfde6e980bde1826c381
Elizaveta <izziantiya@gmail.com>
2021-05-24 01:50:17 +0300

    Add:authorization with Spring Security

---
+++ src/main/java/com/store/service/UserService.java
@@ -0,0 +1,24 @@
+package com.store.service;
+
+import com.store.entity.User;
+import com.store.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUser(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+}
